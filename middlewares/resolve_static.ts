fa38243e14961cba9e7d48b2c63069da6dee1a67
Julien <xdrz.jot.sr@gmail.com>
2022-03-08 23:12:13 +0100

    fix: mime types on static files

--- middlewares/resolve_static.ts
+++ middlewares/resolve_static.ts
@@ -1,4 +1,6 @@
 import { Context, Next } from '../deps.ts'
+import { lookup } from "https://deno.land/x/mime_types@1.0.0/mod.ts";
+
 
 export async function resolveStatic(ctx: Context, next: Next) {
 	const root = `${Deno.cwd()}/public`
@@ -11,6 +13,8 @@ export async function resolveStatic(ctx: Context, next: Next) {
 	try {
 		ctx.response.body = await Deno.readFile(filePath)
 		ctx.response.status = 200
+		const mimeType = lookup(filePath)
+		ctx.response.type = mimeType ? mimeType : undefined
 	} catch {
 		ctx.response.body = 'Fichier introuvable'
 		ctx.response.status = 404
